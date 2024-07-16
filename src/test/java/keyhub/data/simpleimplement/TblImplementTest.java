9add9a39cc31c794b1a5f8a16196c28b3b5c87cf
lshh <markhong93@naver.com>
2024-07-16 11:41:16 +0900

    update test

--- src/test/java/keyhub/data/simpleimplement/TblImplementTest.java
+++ src/test/java/keyhub/data/simpleimplement/TblImplementTest.java
@@ -20,25 +20,24 @@ public class TblImplementTest {
     public class ConvertTest{
         @Test
         public void TestConvertToValue(){
-            Tbl tbl = new TblVariable(List.of("id", "name"))
+            TblVariable tbl = new TblVariable(List.of("id", "name"))
                     .addRow(List.of(1, "aaa"))
                     .addRow(List.of(2, "bbb"))
                     .addRow(List.of(3, "ccc"));
 
             assertEquals(tbl.getClass(), TblVariable.class);
-            TblVariable tblVariable = (TblVariable) tbl;
-            assertEquals(tbl.getColumns(), tblVariable.getColumns());
-            assertEquals(tbl.getRows(), tblVariable.getRows());
+            assertEquals(tbl.getColumns(), tbl.getColumns());
+            assertEquals(tbl.getRows(), tbl.getRows());
 
-            TblValue tblValue = tblVariable.toValue();
+            TblValue tblValue = tbl.toValue();
             assertEquals(tbl.getColumns(), tblValue.getColumns());
             assertEquals(tbl.getRows(), tblValue.getRows());
-            assertEquals(tblVariable.getColumns(), tblValue.getColumns());
+            assertEquals(tbl.getColumns(), tblValue.getColumns());
         }
 
         @Test
         public void testConvertToVariable(){
-            Tbl tbl = Tbl.builder()
+            TblValue tbl = Tbl.builder()
                     .addColumns(List.of("id", "name"))
                     .addRows(List.of(
                         List.of(1, "aaa"),
@@ -48,14 +47,13 @@ public class TblImplementTest {
                     .build();
 
             assertEquals(tbl.getClass(), TblValue.class);
-            TblValue tblValue = (TblValue) tbl;
-            assertEquals(tbl.getColumns(), tblValue.getColumns());
-            assertEquals(tbl.getRows(), tblValue.getRows());
+            assertEquals(tbl.getColumns(), tbl.getColumns());
+            assertEquals(tbl.getRows(), tbl.getRows());
 
-            TblVariable tblVariable = tblValue.toVariable();
+            TblVariable tblVariable = tbl.toVariable();
             assertEquals(tbl.getColumns(), tblVariable.getColumns());
             assertEquals(tbl.getRows(), tblVariable.getRows());
-            assertEquals(tblValue.getColumns(), tblVariable.getColumns());
+            assertEquals(tbl.getColumns(), tblVariable.getColumns());
         }
     }
 
@@ -95,6 +93,78 @@ public class TblImplementTest {
             // 4
             assertEquals(4, result.getRow(0).size());
         }
+
+        @Test
+        @DisplayName("Test Inner Join Success")
+        public void testInnerJoin2(){
+            Tbl left = Tbl.builder()
+                    .addColumns(List.of("id", "name"))
+                    .addRow(List.of(1, "Alice"))
+                    .addRow(List.of(2, "Bob"))
+                    .addRow(List.of(3, "Charlie"))
+                    .build();
+
+            Tbl right = Tbl.builder()
+                    .addColumns(List.of("id", "age"))
+                    .addRow(List.of(1, 20))
+                    .addRow(List.of(3, 30))
+                    .addRow(List.of(4, 40))
+                    .build();
+
+            JoinSet joinSet = InnerJoinSet.of(left, right)
+                    .on("id")
+                    .selectAllFromLeft()
+                    .selectFromRight("age");
+
+            Tbl result = joinSet.toTbl();
+
+            System.out.println(result.getColumns());
+            System.out.println(result.getRows());
+            // 2
+            // [id, name, age]
+            assertEquals(3, result.getColumns().size());
+            // 2
+            // [[1, Alice, 20], [3, Charlie, 30]]
+            assertEquals(2, result.getRows().size());
+            // 2
+            assertEquals(3, result.getRow(0).size());
+        }
+
+        @Test
+        @DisplayName("Test Inner Join Success")
+        public void testInnerJoin3(){
+            Tbl left = Tbl.builder()
+                    .addColumns(List.of("id", "name"))
+                    .addRow(List.of(1, "Alice"))
+                    .addRow(List.of(2, "Bob"))
+                    .addRow(List.of(3, "Charlie"))
+                    .build();
+
+            Tbl right = Tbl.builder()
+                    .addColumns(List.of("id", "age"))
+                    .addRow(List.of(1, 20))
+                    .addRow(List.of(3, 30))
+                    .addRow(List.of(4, 40))
+                    .build();
+
+            JoinSet joinSet = InnerJoinSet.of(left, right)
+                    .on("id")
+                    .selectFromLeft("name", "id")
+                    .selectFromRight("age", "id");
+
+            Tbl result = joinSet.toTbl();
+
+            System.out.println(result.getColumns());
+            System.out.println(result.getRows());
+            // 2
+            // [name, id, age, id]
+            assertEquals(4, result.getColumns().size());
+            // 2
+            // [[1, Alice, 20, 1], [3, Charlie, 30, 3]]
+            assertEquals(2, result.getRows().size());
+            // 2
+            assertEquals(4, result.getRow(0).size());
+        }
     }
 
     @Nested
@@ -128,6 +198,78 @@ public class TblImplementTest {
             assertEquals(left.getColumnSize()+ right.getColumnSize(), result.getColumns().size());
             // 3
             // [[1, Alice, 1, 20], [2, Bob, null, null], [3, Charlie, 3, 30]]
+            assertEquals(left.size(), result.getRows().size());
+            // 4
+            assertEquals(4, result.getRow(0).size());
+        }
+
+        @Test
+        @DisplayName("Test Left Join Success")
+        public void testLeftJoin2(){
+            Tbl left = Tbl.builder()
+                    .addColumns(List.of("id", "name"))
+                    .addRow(List.of(1, "Alice"))
+                    .addRow(List.of(2, "Bob"))
+                    .addRow(List.of(3, "Charlie"))
+                    .build();
+
+            Tbl right = Tbl.builder()
+                    .addColumns(List.of("id", "age"))
+                    .addRow(List.of(1, 20))
+                    .addRow(List.of(3, 30))
+                    .addRow(List.of(4, 40))
+                    .build();
+
+            JoinSet joinSet = LeftJoinSet.of(left, right)
+                    .on("id")
+                    .selectAllFromLeft()
+                    .selectFromRight("age");
+
+            Tbl result = joinSet.toTbl();
+
+            System.out.println(result.getColumns());
+            System.out.println(result.getRows());
+            // 2
+            // [id, name, age]
+            assertEquals(3, result.getColumns().size());
+            // 3
+            // [[1, Alice, 20], [2, Bob, null], [3, Charlie, 30]]
+            assertEquals(left.size(), result.getRows().size());
+            // 3
+            assertEquals(3, result.getRow(0).size());
+        }
+
+        @Test
+        @DisplayName("Test Left Join Success")
+        public void testLeftJoin3(){
+            Tbl left = Tbl.builder()
+                    .addColumns(List.of("id", "name"))
+                    .addRow(List.of(1, "Alice"))
+                    .addRow(List.of(2, "Bob"))
+                    .addRow(List.of(3, "Charlie"))
+                    .build();
+
+            Tbl right = Tbl.builder()
+                    .addColumns(List.of("id", "age"))
+                    .addRow(List.of(1, 20))
+                    .addRow(List.of(3, 30))
+                    .addRow(List.of(4, 40))
+                    .build();
+
+            JoinSet joinSet = LeftJoinSet.of(left, right)
+                    .on("id")
+                    .selectFromLeft("name", "id")
+                    .selectFromRight("age", "id");
+
+            Tbl result = joinSet.toTbl();
+
+            System.out.println(result.getColumns());
+            System.out.println(result.getRows());
+            // 2
+            // [name, id, age, id]
+            assertEquals(4, result.getColumns().size());
+            // 3
+            // [[1, Alice, 20, 1], [2, Bob, null, null], [3, Charlie, 30, 3]]
             assertEquals(left.size(), result.getRows().size());
             // 4
             assertEquals(4, result.getRow(0).size());
